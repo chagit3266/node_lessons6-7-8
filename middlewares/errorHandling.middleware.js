ef4e75e64664a7c79617de8db883c650d88e8340
chagit levi <chagit3266@gmail.com>
2025-06-16 18:56:09 +0300

    Update errorHandling.middleware.js

--- middlewares/errorHandling.middleware.js
+++ middlewares/errorHandling.middleware.js
@@ -5,8 +5,7 @@
  * @param {*} next 
  */
 export const notFound = (req, res, next) => {
-    // כאשר שולחים פרמטר לנקסט
-    // עוברים אוטומטית למידלוואר של השגיאות
+    
     next({ message: 'url not found!', status: 404 });
 }
 /**
@@ -15,6 +14,8 @@ export const notFound = (req, res, next) => {
  * @param {Response} res 
  * @param {() => void} next 
  */
+// כאשר שולחים פרמטר לנקסט
+// עוברים אוטומטית למידלוואר של השגיאות
 export const errorHandler = (err, req, res, next) => {
    res.status(err.status||500).json({err:(err.message||'Server Error')+'1111111111'})
 }
